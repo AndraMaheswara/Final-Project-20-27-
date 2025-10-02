a562b79af87fd4d1d4adf683f0b9f5211d99870a
AndraMaheswara <andram4heswara@gmail.com>
2025-10-02 15:27:43 +0700

    aaa

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini baris pertama saya27");
-        System.out.println("User_27 menambahkan baris baru mpfghh.");
+        System.out.println("User_27 menambahkan baris baru.");
     }
 }
